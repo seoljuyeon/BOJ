e976058be3467f77ddda552a87369a667a4a3b92
seoljuyeon <sxxljxxxxn@gmail.com>
2023-04-22 02:04:12 +0900

    1

---
+++ src/Stage1/BOJ1001.java
@@ -0,0 +1,14 @@
+package Stage1;
+
+import java.util.Scanner;
+
+public class BOJ1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A-B);
+    }
+}
---
+++ src/Stage1/BOJ2557.java
@@ -0,0 +1,7 @@
+package Stage1;
+
+public class BOJ2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
