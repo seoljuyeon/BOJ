7e0325524d46c453e9d55233f3435093b9c0101e
seoljuyeon <sxxljxxxxn@gmail.com>
2023-04-22 02:14:44 +0900

    2

--- src/Stage1/BOJ10998.java
+++ src/Stage1/BOJ10998.java
@@ -1,13 +1,13 @@
 package Stage1;
 import java.util.Scanner;
 
-public class BOJ1001 {
+public class BOJ10998 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int A = scanner.nextInt();
         int B = scanner.nextInt();
 
-        System.out.println(A-B);
+        System.out.println(A*B);
     }
 }
